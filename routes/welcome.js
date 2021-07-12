fd8095183018fb10eed81e1e27db0c62ee0e50db
PamelaSohWY <pamelasohwy@gmail.com>
2021-07-12 08:09:22 +0000

    created welcome page and set up welcome route

--- routes/welcome.js
+++ routes/welcome.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 //#new route added 
 router.get('/', (req,res)=>{
-    res.send("This is the landing and welcome place")
+    // res.send("This is the landing and welcome place")//this is for testing 
+    //change this to render the index.hbs in welcome folder in views 
+    res.render('welcome/index')
 })
 
 //router object is exported 

46ef091384154677379b96a7accefd65564fd3a6
PamelaSohWY <pamelasohwy@gmail.com>
2021-07-12 08:20:39 +0000

    created aboutcompany and contactcompany hbs , and also added the routes to welcome.js in routes . working

--- routes/welcome.js
+++ routes/welcome.js
@@ -2,11 +2,20 @@ const express = require ("express");
 //#1 Create a New Express Router 
 const router = express.Router(); 
 
-//#new route added 
+//ROUTEST TO BE ADDED 
 router.get('/', (req,res)=>{
     // res.send("This is the landing and welcome place")//this is for testing 
     //change this to render the index.hbs in welcome folder in views 
     res.render('welcome/index')
+})
+//new route to contactcompany.hbs in welcome folder 
+router.get('/contactcompany', (req,res)=> { //the params
+    res.render('welcome/contactcompany') //the folder/file name
+})
+
+//new route to aboutcompany.hbs in welcome folder 
+router.get('/aboutcompany', (req,res) => {
+    res.render('welcome/aboutcompany')
 })
 
 //router object is exported 
